729ce1d407c45fa87fa5edc3e19449990de17898
BinniPyx <aleks.kalinichenko.98@mail.ru>
2021-07-15 13:55:46 +0300

    Update gitignore

--- MyServer.ts
+++ MyServer.ts
@@ -1,4 +1,4 @@
-
+let fs=require('fs')
 let express=require('express')
 const {MongoClient} = require('mongodb');
 let app=express()
@@ -10,13 +10,23 @@ MongoClient.connect('mongodb+srv://XiLLeR:Kalina270698@cluster0.qckeb.mongodb.ne
     }
     else{
 
-        app.listen(80,()=>{
-            console.log('Started')
+        app.listen(8000,()=>{
+            console.log('Started up')
         })
     }
 
 })
 
-app.get('/',(req,res)=>{
-    res.sendStatus(200)
+app.get('/u',(req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile('./build/index.html',null,(err,data)=>{
+        if(err){
+            res.sendStatus(404)
+        }
+        else{
+            res.write('Write')
+            res.write(data)
+        }
+
+    })
 })
